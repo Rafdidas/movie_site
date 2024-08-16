89cdf91eff5cebd97eeebfc7598152d7f68f4171
hue <hue@wisa.co.kr>
2024-08-16 16:19:34 +0900

    footer edit

--- src/js/footer.js
+++ src/js/footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer>
             <div className="wrap_inner">
                 <p className="copyright">Designed & developed by Hyeongyu Park</p>
-                <p className="github"><a href="https://github.com/Rafdidas/movie_site" target="_blank" rel="noopener noreferrer">View Code</a></p>
+                <p className="github"><a href="https://github.com/Rafdidas/movie_db" target="_blank" rel="noopener noreferrer">View Code</a></p>
             </div>
         </footer>
     );
